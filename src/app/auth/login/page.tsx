91be418b73f9ae27eb56204af8c911e9bbffb636
Herzog <mauroduques01@gmail.com>
2025-08-19 21:33:52 -0600

    Account Linking in progress

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,5 +1,6 @@
 import { LoginForm } from "@/components/ui/login-form";
 import { ReturnButton } from "@/components/ui/return-button";
+import { SignInOAuthButton } from "@/components/ui/sign-in-oauth-button";
 import Link from "next/link";
 
 export default function Page() {
@@ -10,13 +11,22 @@ export default function Page() {
         <h1 className="text-3xl font-bold">Login</h1>
       </div>
 
-      <LoginForm />
-      <p className="text-muted-foreground text-sm">
-        Don't have an account?{" "}
-        <Link href="/auth/register" className="text-blue-600 hover:underline">
-          Register
-        </Link>
-      </p>
+      <div className="text-muted-foreground test-sm">
+        <LoginForm />
+        <p className="text-muted-foreground text-sm">
+          Don't have an account?{" "}
+          <Link href="/auth/register" className="text-blue-600 hover:underline">
+            Register
+          </Link>
+        </p>
+
+        <hr className="max-w-sm" />
+      </div>
+
+      <div className="flex flex-col max-w-sm gap-4">
+        <SignInOAuthButton provider="google" />
+        <SignInOAuthButton provider="github" />
+      </div>
     </div>
   );
 }
--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,5 +1,6 @@
 import { RegisterForm } from "@/components/ui/register-form";
 import { ReturnButton } from "@/components/ui/return-button";
+import { SignInOAuthButton } from "@/components/ui/sign-in-oauth-button";
 import Link from "next/link";
 
 export default function Page() {
@@ -10,13 +11,22 @@ export default function Page() {
         <h1 className="text-3xl font-bold">Register</h1>
       </div>
 
+      <div className="text-muted-foreground test-sm">
         <RegisterForm />
         <p className="text-muted-foreground text-sm">
-        Already have an account?{" "}
-        <Link href="/auth/login" className="text-blue-600 hover:underline">
-          Login
-        </Link>
-      </p>
+          Already have an account?{" "}
+          <Link href="/auth/login" className="text-blue-600 hover:underline">
+            Login
+          </Link>
+        </p>
+
+        <hr className="max-w-sm" />
+      </div>
+
+      <div className="flex flex-col max-w-sm gap-4">
+        <SignInOAuthButton signUp provider="google" />
+        <SignInOAuthButton signUp provider="github" />
+      </div>
     </div>
   );
 }
